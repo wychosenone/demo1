76488fdfe6c7833f0ff368f9081df352aee390dc
aaronwang <wy19980310@gmail.com>
2022-09-21 19:19:26 -0700

    push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from './Components/Todo';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <h1>My todos</h1>
+    <Todo text='Learn React' />
+    <Todo text='Master React' />
+    <Todo text='Explore the Depth' />
+    <Todo text='Yue is gonna Win' />
+  </div>;
 }
 
 export default App;
